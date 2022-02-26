17a2eebcedc6a13201d0308066ba5a5a0171e861
Alexis <amart78@outlook.com>
2022-02-26 16:20:47 -0600

    commit tree 185cc993e8a9b23f7032da26c88ead656dc8fe47

---
+++ index.js
@@ -0,0 +1,80 @@
+//receivesAFunction
+
+function receivesAFunction(cb){
+    cb();
+}
+
+//returnsANamedFunction
+// - take no arguments
+// - return a _named function_
+
+function returnsANamedFunction(){
+    return function namedFunction(){
+        //blank
+    }
+}
+
+//returnsAnAnonymousFuncion
+// - take no arguments
+// - return an _anonymous function_
+
+function returnsAnAnonymousFunction(){
+    return function (){}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function receivesAFunction(cb){
+//     cb();
+// }
+
+// function aNamedFunction(){
+// }
+
+// function returnsANamedFunction(){
+//     return aNamedFunction;
+// }
+
+// function returnsAnAnonymousFunction(){
+//     return () => {}
+//     const foo = () => 1
+
+//         foo()
+// }
+
+
